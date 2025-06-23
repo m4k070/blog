6e140252936fe84102d9279064bde9f3ae260e92
Makoto Ito <m4k070@pm.me>
2025-06-23 21:51:08 +0900

    reset

--- Program.fs
+++
@@ -1,7 +0,0 @@
-﻿open Browser.Dom
-open Feliz
-open App
-
-let elm = document.getElementById "root"
-let root = ReactDOM.createRoot (elm)
-root.render (App())
--- lib/Parser.fs
+++
@@ -1,9 +0,0 @@
-module Parser
-
-open Fable.Core
-
-type IAsciidoctor = 
-    abstract convert: string -> string
-
-[<ImportDefault("asciidoctor")>]
-let asciidoctor: unit -> IAsciidoctor = jsNative
